a87d41f55a3bef94a9a6165024115a4a54b31cbc
Chathura Theekshana <chathuradayananda1@gmail.com>
2024-10-03 02:13:09 +0530

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,58 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:spm_shoppingmall_mobile/lockerFunction/pages/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+// Remove the 'firebase_options.dart' import
+import '../eventsFunction/screens/home_screen.dart';
+import '../eventsFunction/screens/events_screen.dart';
+import '../eventsFunction/screens/favorites_screen.dart';
+import '../eventsFunction/screens/settings_screen.dart';
+import '../eventsFunction/widgets/navbar.dart';
 
-Future main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  await prefs.setString('userID', 'C001');
+  await Firebase.initializeApp(); // No options passed here
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      /* title: 'Flutter Firebase auth',
+      title: 'Mall Events App',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
-        hintColor: Colors.amber,
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-              fontSize: 32, fontWeight: FontWeight.bold, color: Colors.black),
-          bodyLarge: TextStyle(fontSize: 16, color: Colors.grey),
-        ),
-        buttonTheme: const ButtonThemeData(
-          buttonColor: Colors.teal,
-          shape: RoundedRectangleBorder(),
-        ),
-      ), */
-      initialRoute: '/home',
-      routes: {'/home': (context) => const Home()},
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    HomeScreen(),
+    EventsScreen(),
+    FavoritesScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: Navbar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
