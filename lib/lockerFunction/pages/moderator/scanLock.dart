c5e0dfa4f45dcbc7c3844928715bf550b1f2a148
ChamodVaas <it22334138@my.sliit.lk>
2024-10-04 09:27:34 +0530

    ui updated

--- lib/lockerFunction/pages/moderator/scanLock.dart
+++ lib/lockerFunction/pages/moderator/scanLock.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:spm_shoppingmall_mobile/lockerFunction/service/database.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -12,55 +13,202 @@ class ScanLock extends StatefulWidget {
 class _ScanLockState extends State<ScanLock> {
   String scanResult = '';
   final MobileScannerController cameraController = MobileScannerController();
+  bool isScanning = false; // To track whether we are in scan mode
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue, // Set background color to blue
       appBar: AppBar(
         title: const Text('Scan QR Code'),
-        actions: [
-          IconButton(
-            icon: Icon(cameraController.torchEnabled
-                ? Icons.flash_on
-                : Icons.flash_off),
-            color: Colors.white,
-            onPressed: () => cameraController.toggleTorch(),
-          ),
-          IconButton(
-            icon: const Icon(Icons.cameraswitch),
-            onPressed: () => cameraController.switchCamera(), // Switch camera without needing cameraFacing getter
-          ),
-        ],
+        backgroundColor: Colors.blue, // Match the background color
+        elevation: 0, // Remove shadow for cleaner look
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: MobileScanner(
-              controller: cameraController,
-              onDetect: (capture) {
-                final List<Barcode> barcodes = capture.barcodes;
-                final String? barcode = barcodes.isNotEmpty
-                    ? barcodes.first.rawValue
-                    : null;
-                if (barcode != null) {
-                  setState(() {
-                    scanResult = barcode;
-                  });
-                  cameraController.stop(); // Stop scanning when QR is detected
-                  _showConfirmationDialog(context, barcode);
-                }
-              },
-            ),
+      body: Center(
+        child: Container(
+          width: 350,
+          height: 600, // Set box width
+          padding: const EdgeInsets.all(16), // Padding inside the box
+          decoration: BoxDecoration(
+            color: Colors.white, // Set box background color
+            borderRadius: BorderRadius.circular(20), // Rounded corners
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                offset: Offset(0, 5),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Scan Result: $scanResult',
-              style: const TextStyle(fontSize: 16),
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Only take minimum space
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Text message above the camera/QR code
+              Text(
+                isScanning
+                    ? "Place QR code inside the box"
+                    : "Click the scan button to scan",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black, // Match text color with theme
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Stack(
+                children: [
+                  Padding( // Add padding around the camera/placeholder to create space
+                    padding: const EdgeInsets.all(12.0), // Adjust the padding to set distance
+                    child: SizedBox(
+                      width: 250,
+                      height: 250,
+                      child: isScanning
+                          ? MobileScanner(
+                              controller: cameraController,
+                              onDetect: (capture) {
+                                final List<Barcode> barcodes = capture.barcodes;
+                                final String? barcode = barcodes.isNotEmpty
+                                    ? barcodes.first.rawValue
+                                    : null;
+                                if (barcode != null) {
+                                  setState(() {
+                                    scanResult = barcode;
+                                  });
+                                  cameraController.stop(); // Stop scanning when QR is detected
+                                  _showConfirmationDialog(context, barcode);
+                                }
+                              },
+                            )
+                          : Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: QrImageView(
+                                data: 'Wellcome',
+                                errorCorrectionLevel: QrErrorCorrectLevel.H,
+                                version: QrVersions.auto,
+                                gapless: true,
+                              ),
+                            ),
+                    ),
+                  ),
+                  // Highlighted Corners
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    child: _topLeftCornerHighlight(),
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: _topRightCornerHighlight(),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    child: _bottomLeftCornerHighlight(),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: _bottomRightCornerHighlight(),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              if (!isScanning)
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isScanning = true; // Switch to scan mode
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue, // Match background color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text('Scan'),
+                ),
+              if (isScanning)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        cameraController.torchEnabled
+                            ? Icons.flash_on
+                            : Icons.flash_off,
+                      ),
+                      color: Colors.blue,
+                      onPressed: () => cameraController.toggleTorch(),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.cameraswitch),
+                      color: Colors.blue,
+                      onPressed: () => cameraController.switchCamera(),
+                    ),
+                  ],
+                ),
+            ],
           ),
-        ],
+        ),
+      ),
+    );
+  }
+
+  // Corner highlight widget
+  Widget _topRightCornerHighlight() {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(width: 4.0, color: Colors.black),
+          right: BorderSide(width: 4.0, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  Widget _bottomRightCornerHighlight() {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 4.0, color: Colors.black),
+          right: BorderSide(width: 4.0, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  Widget _bottomLeftCornerHighlight() {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 4.0, color: Colors.black),
+          left: BorderSide(width: 4.0, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  Widget _topLeftCornerHighlight() {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(width: 4.0, color: Colors.black),
+          left: BorderSide(width: 4.0, color: Colors.black),
+        ),
       ),
     );
   }
@@ -94,7 +242,7 @@ class _ScanLockState extends State<ScanLock> {
   }
 }
 
-void reserveLock(String barcode){
+void reserveLock(String barcode) {
   DatabaseMethods().getEmptyLocksStream("P101").first.then((snapshot) {
     if (snapshot.docs.isNotEmpty) {
       // Retrieve the first lock's lockId
